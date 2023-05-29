661d679b2b7fc32d80f70e9bca61ecc0ac610d39
Evan Sprecher <EvanSprecher7@gmail.com>
2023-05-29 14:25:50 -0500

    5/29/2023: Initial commit of tables_queries.sql

---
+++ tables_queries.sql
@@ -0,0 +1,47 @@
+-- List the employee number, last name, first name, sex, and salary of each employee (2 points)
+
+
+
+
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986 (2 points)
+
+
+
+
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name (2 points)
+
+
+
+
+
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name (2 points)
+
+
+
+
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B (2 points)
+
+
+
+
+
+-- List each employee in the Sales department, including their employee number, last name, and first name (2 points)
+
+
+
+
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name (4 points)
+
+
+
+
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) (4 points)
+
+
+
+
